eebe43b6d4bc1993ee3e9777fd91ec19efa2afd9
kente <kent.david.embile@gmail.com>
2024-12-13 12:00:28 +0800

    Initial commit

---
+++ demo-spring-feature-flags/src/main/java/com/kentisthebest/DemoSpringFeatureFlagsApplication.java
@@ -0,0 +1,13 @@
+package com.kentisthebest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringFeatureFlagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringFeatureFlagsApplication.class, args);
+	}
+
+}
